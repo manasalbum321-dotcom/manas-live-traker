5d14dbbdcdfecf498a64309592c6cb38e6a93e2a
MANAS PRAJAPATI <manasalbum321@gmail.com>
2026-07-30 06:46:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 6589,
-    username: "admin",
-    password: "admin",
+    username: "manas.in",
+    password: "manas.in",
     token: "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"
 }
